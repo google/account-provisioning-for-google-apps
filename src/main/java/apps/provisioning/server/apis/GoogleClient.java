e31a998b011b33be098028bc66cbd94a89c19f47
Carlos Toxtli <ctoxtli@gmail.com>
2017-11-03 12:23:13 -0400

    JSON Service Account enabled

--- src/main/java/apps/provisioning/server/apis/GoogleClient.java
+++ src/main/java/apps/provisioning/server/apis/GoogleClient.java
@@ -14,14 +14,14 @@
  * limitations under the License.
  */
 
-package apps.provisioning.server.apis;
+package apps.provisioning.server.apis.apps.provisioning.server.apis;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
-
-import apps.provisioning.config.ConfigData;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.http.HttpTransport;
@@ -31,69 +31,90 @@ import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.services.admin.directory.Directory;
 import com.google.api.services.admin.directory.DirectoryScopes;
 
+import apps.provisioning.config.ConfigData;
+
 /**
  * Creates credentials for the Java Google Client.
  */
 public class GoogleClient {
 
-  public HttpTransport httpTransport = new NetHttpTransport();
-  public JsonFactory jsonFactory = new JacksonFactory();
+	public HttpTransport httpTransport = new NetHttpTransport();
+	public JsonFactory jsonFactory = new JacksonFactory();
 
-  protected String domain;
-  protected String authUser;
-  protected String serviceAccountEmail;
-  protected String keyPath;
-  protected String appName;
+	protected String domain;
+	protected String authUser;
+	protected String serviceAccountEmail;
+	protected String keyPath;
+	protected String appName;
 
-  /**
-   * Creates a Google API Client using the credentials stored in the
-   * configuration file.
-   *
-   * @throws IOException
-   */
-  public GoogleClient(ConfigData config) throws IOException, Exception {
-    domain = config.getDomain();
-    authUser = config.getAuthUser();
-    serviceAccountEmail = config.getServiceAccountEmail();
-    keyPath = config.getKeyPath();
-    appName = config.getAppName();
-  }
+	/**
+	 * Creates a Google API Client using the credentials stored in the configuration
+	 * file.
+	 *
+	 * @throws IOException
+	 */
+	public GoogleClient(ConfigData config) throws IOException, Exception {
+		domain = config.getDomain();
+		authUser = config.getAuthUser();
+		serviceAccountEmail = config.getServiceAccountEmail();
+		keyPath = config.getKeyPath();
+		appName = config.getAppName();
+	}
 
-  /**
-   * Constructor used for testing only.
-   */
-  public GoogleClient() {}
+	/**
+	 * Constructor used for testing only.
+	 */
+	public GoogleClient() {
+	}
 
-  /**
-   * Retrieves the service account credentials.
-   *
-   * @param serviceAccountEmail Service email from the Google Developer Console
-   *        project.
-   * @param keyPath Where the p12 file is stored.
-   * @return GoogleCredential object with the active section.
-   * @throws GeneralSecurityException
-   * @throws IOException
-   */
-  protected GoogleCredential getCredentialForServiceAccount(String serviceAccountEmail,
-      String keyPath) throws GeneralSecurityException, IOException {
-    return new GoogleCredential.Builder().setTransport(httpTransport).setJsonFactory(jsonFactory)
-        .setServiceAccountId(serviceAccountEmail)
-        .setServiceAccountScopes(Collections.singleton(DirectoryScopes.ADMIN_DIRECTORY_USER))
-        .setServiceAccountPrivateKeyFromP12File(new File(keyPath)).setServiceAccountUser(authUser)
-        .build();
-  }
+	/**
+	 * Retrieves the service account credentials.
+	 *
+	 * @param serviceAccountEmail
+	 *            Service email from the Google Developer Console project.
+	 * @param keyPath
+	 *            Where the p12 file is stored.
+	 * @return GoogleCredential object with the active section.
+	 * @throws GeneralSecurityException
+	 * @throws IOException
+	 */
+	protected GoogleCredential getCredentialForServiceAccount(String serviceAccountEmail, String keyPath)
+			throws GeneralSecurityException, IOException {
+		return new GoogleCredential.Builder().setTransport(httpTransport).setJsonFactory(jsonFactory)
+				.setServiceAccountId(serviceAccountEmail)
+				.setServiceAccountScopes(Collections.singleton(DirectoryScopes.ADMIN_DIRECTORY_USER))
+				.setServiceAccountPrivateKeyFromP12File(new File(keyPath)).setServiceAccountUser(authUser).build();
+	}
 
-  /**
-   * Create a new authorized Google API client.
-   *
-   * @param projectName The project name that is displayed in the Google
-   *        Developer Console.
-   * @param credential The GoogleCredential object.
-   * @return The Admin SDK client object
-   */
-  protected Directory createAuthorizedClient(String projectName, GoogleCredential credential) {
-    return new Directory.Builder(httpTransport, jsonFactory, credential).setApplicationName(
-        projectName).build();
-  }
+	protected GoogleCredential getCredential(String serviceAccountEmail, String keyPath)
+			throws GeneralSecurityException, IOException { 
+		InputStream credentialStream = new FileInputStream(keyPath);
+		GoogleCredential credentialJson = GoogleCredential.fromStream(credentialStream, httpTransport, jsonFactory)
+				.createScoped(Collections.singleton(DirectoryScopes.ADMIN_DIRECTORY_USER));
+
+		GoogleCredential credential = new GoogleCredential.Builder()
+				.setTransport(credentialJson.getTransport())
+				.setJsonFactory(credentialJson.getJsonFactory())
+				.setServiceAccountId(credentialJson.getServiceAccountId())
+				.setServiceAccountUser(authUser)
+				.setServiceAccountPrivateKey(credentialJson.getServiceAccountPrivateKey())
+				.setServiceAccountScopes(credentialJson.getServiceAccountScopes())
+				.build();
+		return credential;
+	}
+
+	/**
+	 * Create a new authorized Google API client.
+	 *
+	 * @param projectName
+	 *            The project name that is displayed in the Google Developer
+	 *            Console.
+	 * @param credential
+	 *            The GoogleCredential object.
+	 * @return The Admin SDK client object
+	 */
+	protected Directory createAuthorizedClient(String projectName, GoogleCredential credential) {
+		return new Directory.Builder(httpTransport, jsonFactory, credential).setApplicationName(projectName).build();
+	}
 
 }
